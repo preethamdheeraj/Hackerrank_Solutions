d8a18769801f60ae3f475f952f30eb98278cd768
preethamdheeraj <chpreetham45@gmail.com>
2023-05-03 08:54:12 +0530

    testing for .DS_Store upload to main

--- HackerRankPythonsolutions/14what's your name.py
+++ HackerRankPythonsolutions/14what's your name.py
@@ -8,3 +8,5 @@ if __name__ == '__main__':
     first_name = input()
     last_name = input()
     print_full_name(first_name, last_name)
+
+# adding some comments to make changes and see if the .DS_Store file is not going to be tracked
